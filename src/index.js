9499688262593c29b19e6fcfb798832a261c2b93
Yury <chifius@mail.ru>
2020-03-22 22:47:11 +0300

    feat: finish Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+    let brCnfg = bracketsConfig.map((s) => s.join(""));
+  
+    for (let i = 0; i < brCnfg.length; i++) {
+      if (str.includes(brCnfg[i])) {
+        str = str.replace(brCnfg[i], "");
+        i = -1;
+      }
+    }
+      
+    if (str == '') {
+      return true;
+    } else {
+      return false; 
+    }
+  }
